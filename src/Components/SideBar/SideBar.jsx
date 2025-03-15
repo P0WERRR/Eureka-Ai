033b4d3f3a43a4c581196bb230ac8c3d53feeebe
Aditya Raj Bundela <99018925+P0WERRR@users.noreply.github.com>
2025-03-15 12:47:54 +0530

    second commit

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -5,35 +5,33 @@ import { Context } from '../../Context/Context';
 
 const SideBar = () => {
   const [extended, setExtended] = useState(false);
-  const {onSent,prevPrompts,setRecentPrompt ,newChat} = useContext(Context)
-  
+  const { onSent, prevPrompts, setRecentPrompt, newChat } = useContext(Context)
+
   const loadPrompt = async (prompt) => {
     setRecentPrompt(prompt)
     await onSent(prompt)
   }
-  
+
   return (
     <>
-      
       <div className='sidebar'>
         <div className='top'>
-            <img className='menu' onClick={() => setExtended(prev => !prev)}  src={assets.menu_icon} alt="" />
-            <div onClick={()=>newChat()} className='new-chat'>
-                <img className='icon' src={assets.plus_icon} alt="" />
-                {extended ? <p>New Chat</p> : null}
-            </div>
-          
+          <img className='menu' onClick={() => setExtended(prev => !prev)} src={assets.menu_icon} alt="" />
+          <div onClick={() => newChat()} className='new-chat'>
+            <img className='icon' src={assets.plus_icon} alt="" />
+            {extended ? <p>New Chat</p> : null}
+          </div>
+
           {extended ?
             <div className='recent'>
-              
-              <p className='recent-title' style={{color : '#282828'}}>Recent</p>
-              {prevPrompts.map((item,index)=>{
-                  return(
-                    <div onClick={()=>loadPrompt(item)} className='recent-entry'>
-                      <img className='icon' src={assets.message_icon} alt="" />
-                      <p>{item.slice(0,18)}...</p>
-                    </div>
-                  )
+              <p className='recent-title' style={{ color: '#282828' }}>Recent</p>
+              {prevPrompts.map((item, index) => {
+                return (
+                  <div key={index} onClick={() => loadPrompt(item)} className='recent-entry'>
+                    <img className='icon' src={assets.message_icon} alt="" />
+                    <p>{item.slice(0, 18)}...</p>
+                  </div>
+                )
               })}
             </div> : null}
         </div>
